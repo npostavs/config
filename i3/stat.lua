20f68b68ebd40b92898a7159e8d27b1dcb148510
Noam Postavsky <npostavs@users.sourceforge.net>
2013-07-30 20:12:04 -0400

    the old config, assumes stat.lua in ~/bin

---
+++ i3/stat.lua
@@ -0,0 +1,139 @@
+local ox = require('posix')
+--------------------------------------
+---- stat gathering functions
+--------------------------------------
+local cpus = {}
+
+local function cpu_stats(cpu_line)
+   local id, stats = string.match(cpu_line, '^cpu(%d*)%s+(.+)$')
+   local idle
+   local total = 0
+   local col = 1
+
+   for s in string.gmatch(stats, '(%d+)%s+') do
+      local n = tonumber(s)
+      total = total + n
+      if col == 4 then idle = n end
+      col = col + 1
+   end
+
+   if cpus[id] then
+      local dtotal = total - cpus[id].total
+      local didle = idle - cpus[id].idle
+      cpus[id].usage = 1 - (didle / dtotal)
+   else
+      cpus[id] = { usage = 0 }
+   end
+
+   cpus[id].total = total
+   cpus[id].idle = idle
+end
+
+local function memstats(meminfo)
+   local stats = {}
+   for line in meminfo:lines() do
+      local field, value = string.match(line, '^([^%s:]+):%s*(%d+)')
+      if field and value then
+         stats[field] = tonumber(value)
+      else
+         io.stderr:write(line, '\n')
+      end
+   end
+   local free = stats.MemFree + stats.Buffers + stats.Cached
+   local usage = 1 - free / stats.MemTotal
+   local swap_usage = 1 - stats.SwapFree / stats.SwapTotal
+   return { usage = usage, swap_usage = swap_usage }
+end
+
+local batt_total
+local BATTERY = '/sys/class/power_supply/BAT1'
+do
+   local battinfo = io.open(BATTERY .. '/charge_full')
+   batt_total = tonumber(battinfo:read())
+   battinfo:close()
+end
+
+
+local function battstat(battinfo)
+   info = battinfo:read()
+
+   if info then
+      return tonumber(info) / batt_total
+   else
+      return nil
+   end
+end
+
+
+--------------------------------------
+---- meter drawing functions
+--------------------------------------
+
+local W = 6
+local H = 20
+local function dzen_graph(colour, percent)
+   local h = math.floor(H*percent + 0.5)
+   if percent > 0 and h < H then h = h + 1 end
+   return string.format(
+      '^ib(1)^fg(black)^r(%dx%d)^p(-%d)^fg(%s)^r(%dx%d+0-%d)^fg()^ib(0)',
+      W,H,W,
+      colour, W, h, math.ceil(H/2 + h/2)
+   )
+end
+
+--------------------------------------
+---- main loop
+--------------------------------------
+
+-- we keep these files open, so we don't have to keep reopening them
+-- every second
+local function open_special(filename)
+   local file = io.open(filename)
+   if file then
+      file:setvbuf('no')
+   end
+   return file
+end
+
+local battinfo = open_special(BATTERY .. '/charge_now')
+local stat = open_special('/proc/stat')
+local meminfo = open_special('/proc/meminfo')
+
+while true do
+
+   stat:seek('set')
+   cpu_stats(stat:read())       -- all cpus
+   cpu_stats(stat:read())       -- cpu0
+   cpu_stats(stat:read())       -- cpu1
+
+   meminfo:seek('set')
+   local mstats = memstats(meminfo)
+
+   local batt = nil
+   if battinfo then
+      battinfo:seek('set')
+      batt = battstat(battinfo)
+   end
+   if not batt then
+      battinfo = open_special(BATTERY .. '/charge_now')
+      batt = 0
+   end
+
+   -- io.write(string.format('%d%%batt, ', batt *100))
+   -- io.write(string.format('%d%%mem, %d%%swap, ',
+   --                        mstats.usage *100, mstats.swap_usage *100))
+   -- for id, cpu in pairs(cpus) do
+   --    io.write(string.format('%d%%cpu%s ', cpu.usage *100, id))
+   -- end
+
+   io.write(dzen_graph('blue', mstats.usage))
+   io.write(dzen_graph('purple', mstats.swap_usage))
+   io.write(dzen_graph('green', cpus['0'].usage))
+   io.write(dzen_graph('green', cpus['1'].usage))
+   io.write(dzen_graph('orange', batt))
+   io.write(os.date('%a %b %d, %r'))
+   io.write('\n')
+   io.flush()
+
+   ox.sleep(1)
+end
